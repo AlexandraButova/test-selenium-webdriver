69a61775aee2c9cbfa929d7a49d74c17d721b2c4
Alexandra <bytovaalex@mail.ru>
2022-10-24 13:09:10 +0700

    zadanie 6

--- src/test/java/org/example/LoginLitecart.java
+++ src/test/java/org/example/LoginLitecart.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class LoginLitecart {
     private WebDriver driver;
     private WebDriverWait wait;
@@ -28,6 +30,23 @@ public class LoginLitecart {
         WebElement password = driver.findElement(By.name("password"));
         password.sendKeys("admin");
         driver.findElement(By.xpath("//button[contains(@name, 'login')]")).click();
+    }
+
+    @Test
+    public void homework() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.cssSelector("td [name = username]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("td [name = password]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("div.footer [name = login]")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//ul[@id='box-apps-menu']//li"));
+        // elements.size() кол-во элементов в списке пунктов меню.
+        for (int i = 1; i <= elements.size() ; i++) {
+            driver.findElement(By.xpath("//ul[@id='box-apps-menu']/li["+i+"]")).click();
+            List<WebElement> element = driver.findElements(By.xpath("//ul[@class='docs']/li"));
+            for (int j = 1; j < element.size() + 1; j++){
+                driver.findElement(By.xpath("//ul[@class='docs']//li["+j+"]")).click();
+            }
+        }
     }
 
     @After
